6e623dea9ebb5c8d5eca4b21bc1d66a7a4731c51
vinnyfs89 <viniciusfesil@gmail.com>
2018-05-16 02:29:22 -0300

    :zap: Adding index.php file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+
+define("CLIENT_ID", "YOUR CLIENT ID");
+define("CLIENT_SECRET", "YOUR CLIENT SECRET");
+define("REDIRECT_URL", "YOUR REDIRECT URL");

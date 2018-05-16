2b6dbebda74bfe84ec55c7efac168f4a416b3d7e
vinnyfs89 <viniciusfesil@gmail.com>
2018-05-16 02:32:19 -0300

    :zap: Adding basic strutucture to use Twig as handler of views.

--- index.php
+++ index.php
@@ -8,3 +8,14 @@ $client = new Client();
 define("CLIENT_ID", "YOUR CLIENT ID");
 define("CLIENT_SECRET", "YOUR CLIENT SECRET");
 define("REDIRECT_URL", "YOUR REDIRECT URL");
+
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig() //use twig for handling views
+));
+
+$view = $app->views();
+$views->parserOptions = [
+    'debug' => true, //enable error reporting in the view
+    'cache' => dirname(__FILE__) . '/cache' //set directory for caching views
+];
+
